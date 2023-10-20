4867daf04cc99fb8d7301a58fccd58e09162e69b
steiyve <prigge.nicolas@gmail.com>
2023-10-20 08:02:26 -0400

    creation du system de presence

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "src/creatList.cpp"
 
 using namespace std;
 
 int main()
 {
+    string* classe = creatList();
+    for (int i = 0; i < 40; i++)
+    {
+        cout << classe[i] << endl;
+    }
+    saveList(classe);
+
     cout << "Hello World!" << endl;
     return 0;
 }
