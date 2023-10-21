60bafd11f9538e0690885f7bc90028a627982371
steiyve <prigge.nicolas@gmail.com>
2023-10-21 17:08:37 -0400

    gestion derreur

--- src/prisePresence.cpp
+++ src/prisePresence.cpp
@@ -4,51 +4,60 @@
 
 using namespace std;
 
-void prisePresence()
+int prisePresence()
 {
-    string absance;
     string nomGroupe;
-    string etatEleve;
     string classe[40][2];
 
     cout << "Nom du groupe : ";
     cin >> nomGroupe;
 
     string nomFichier = "var/list" + nomGroupe + ".txt";
-    ifstream file;
-    file.open(nomFichier, ios::in);
-    string line;
+    ifstream file(nomFichier);
 
+    if (!file.is_open()) {
+        cout << "Erreur lors de l'ouverture du fichier" << endl;
+        return -1;
+    }
+
+    string line;
     int pos = 0;
+
     while (getline(file, line)) {
+        if (pos >= 40) {
+            cout << "Erreur: trop d'eleves dans la liste" << endl;
+            return -2;
+        }
 
         classe[pos][0] = line;
         cout << line << "\n Absent ? (oui/non) : ";
+
+        string etatEleve;
         cin >> etatEleve;
 
-        if (etatEleve == "oui"){
-            etatEleve = "absent";
-        }
-        else if (etatEleve == "non"){
-            etatEleve = "present";
-        }
-        else{
+        while (etatEleve != "oui" && etatEleve != "non") {
             cout << "Erreur, veuillez repondre par oui ou non" << endl;
             cin >> etatEleve;
         }
-        
-        classe[pos][1] = etatEleve;
+
+        classe[pos][1] = (etatEleve == "oui") ? "absent" : "present";
         pos++;
     }
+
     file.close();
 
+    ofstream file2(nomFichier);
 
-    ofstream file2;
-    file2.open(nomFichier, ios::trunc);
-    for (int i = 0; i < pos; i++){
-        file2 << classe[i][0] << classe[i][1] << endl;
-
+    if (!file2.is_open()) {
+        cout << "Erreur lors de l'ouverture du fichier" << endl;
+        return -1;
     }
-    file2.close();
 
+    for (int i = 0; i < pos; i++) {
+        file2 << classe[i][0] << classe[i][1] << endl;
+    }
+
+    file2.close();
+    
+    return 0;
 }
